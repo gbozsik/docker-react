a9ec864c45d413e05d14869f6c80c176d32b2f9f
gbozsik <g.bozsik@gmail.com>
2021-12-09 19:09:43 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+         Go Johny gogogo
         </a>
       </header>
     </div>
